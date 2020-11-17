a72ccfc7efcaa3435859bb9f04aca8b60025e164
Guo Zhang <>
2020-11-17 21:54:24 +0800

    create

---
+++ README.md
@@ -0,0 +1,15 @@
+# wechatpay-sdk-py
+
+量潮科技出品的微信支付第三方Python SDK
+
+
+## License
+
+本项目遵循[Apache 2.0](LICENSE)协议。
+
+
+## Contributor
+
+- 发起者：[张果](https://github.com/Guo-Zhang)
+- 维护者：[张果](https://github.com/Guo-Zhang) 、
+- 贡献者：
---
+++ wechatpay_sdk/sign.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+微信支付签名算法业务逻辑的实现
+"""
+
+import base64
+import time
+
+from .utils import gen_random_str, rs256_sign_with_pem, rs256_verify_with_pem
+
+
+# ----- 生成签名 -----
+
+def gen_sign_v2():
+    """
+    生成微信支付V2版本API签名
+    :return:
+    """
+    pass
+
+
+def gen_sign_v3(method, api, body, com_private_key_pem: str):
+    """
+    生成微信支付V3版本API签名
+    :return:
+    """
+    # 构造签名串
+    # https://wechatpay-api.gitbook.io/wechatpay-api-v3/qian-ming-zhi-nan-1/qian-ming-sheng-cheng#gou-zao-qian-ming-chuan
+    current_timestamp_str: str = str(int(time.time()))
+    nonce_str: str = gen_random_str(16)
+    raw_sign_str: str = '\n'.join([method, api, current_timestamp_str, nonce_str, body])
+    # 计算签名值
+    # https://wechatpay-api.gitbook.io/wechatpay-api-v3/qian-ming-zhi-nan-1/qian-ming-sheng-cheng#gou-zao-qian-ming-chuan
+    sign = base64.b64encode(rs256_sign_with_pem(raw_sign_str, com_private_key_pem))
+    return sign
+
+
+# ----- 验证签名 -----
+
+def verify_sign_v3(sign: str, wxp_public_key_pem: str) -> bool:
+    """
+    验证微信支付V3版本API签名
+
+    Ref:
+    - https://wechatpay-api.gitbook.io/wechatpay-api-v3/qian-ming-zhi-nan-1/qian-ming-yan-zheng#yan-zheng-qian-ming
+
+    :return:
+    """
+    # base64解码
+    decoded_sign = base64.b64decode(sign)
+    return rs256_verify_with_pem(decoded_sign, wxp_public_key_pem)
+
