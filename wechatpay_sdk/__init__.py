8eb8f82c62d87a22a5c4332d8ff5a7d6c2f1258f
Guo Zhang <>
2020-11-18 20:09:30 +0800

    feature: 设计Base类

--- wechatpay_sdk/__init__.py
+++ wechatpay_sdk/__init__.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+
+
+from .base import WeChatPayBaseAPIClient
+
+
+class WeChatPayAPIClient(WeChatPayBaseClient):
+    def __init__(self):
+        super(WeChatPayBaseAPIClient, self).__init__()
---
+++ wechatpay_sdk/base.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+from .algorithms import gen_sign_v3, verify_sign_v3
+
+
+class WeChatPayBaseAPIClient(object):
+    def __init__(self, mch_id, serial_no, com_private_key_path):
+        self.mch_id = mch_id
+        self.serial_no = serial_no
+        self.api_root_url_v3 = 'https://api.mch.weixin.qq.com/v3/'
+        self.auth_type_v3 = 'WECHATPAY2-SHA256-RSA2048'
+        with open(com_private_key_path) as f:
+            self.com_private_key_pem = f.read()
+
+    def gen_sign_v3(self, method, api, body):
+        """
+        注意：V3版本接口需要为每次请求量身定做一个签名
+        :param method:
+        :param api:
+        :param body:
+        :return:
+        """
+        return gen_sign_v3(method, api, body, self.com_private_key_pem.encode('utf-8'))
+
+    def request_api_v3(self, method, api, body):
+        # 生成URL
+        url = self.api_root_url_v3 + api
+        # 生成签名
+        sign = gen_sign_v3(method, api, body)
+        # 生成认证信息
+        auth_value = "mchid={mchid},nonce_str={nonce_str}".format(mchid=self.mch_id)  # TODO
+        auth = self.auth_type_v3 + '' + auth_value
+        # 生成请求头
+        headers = {'Authorization': auth}
+        # 发送请求
+        # TODO: POST请求传入body，GET请求body为空
+        response = requests.request(method, url, headers=headers)
+        return response.data
